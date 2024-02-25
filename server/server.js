7e2319e927847b13787c3d61aa5fd1508062164e
patel-bhumit <bhumit667@gmail.com>
2024-02-24 21:11:52 -0500

    added function to open config file and append log with the specified log format to log file

--- server/server.js
+++ server/server.js
@@ -12,6 +12,59 @@ const fs = require("fs");
 const port = process.argv[2] || 3000;
 const connectedClients = new Set(); // Set to store connected client usernames
 
+/*
+* Function: readLogConfig(configFilePath)
+* Parameters: configFilePath - the path to the logging configuration file
+* Description: Reads the logging configuration from a file
+* Return values: The logging configuration as a JSON object
+*/
+function readLogConfig(configFilePath) {
+    try {
+      const configData = fs.readFileSync(configFilePath);
+      return JSON.parse(configData);
+    } catch (err) {
+      console.error("Error reading log config file:", err);
+      return null;
+    }
+  }
+  
+/*
+* Function: function saveLog(logFilePath, logData)
+* Parameters: logFilePath - the path to the log file, logData - the log data to be saved
+* Description: Appends log data to a log file
+* Return values: None
+*/
+function saveLog(logFilePath, logData) {
+    let logMessage = "";
+    const logConfig = readLogConfig(logConfigFilePath);
+    // Check the logging format specified in the configuration
+    switch (logConfig.logFormat.toLowerCase()) {
+        case "json":
+        // Log in JSON format
+        logMessage = JSON.stringify(logData) + "\n";
+        logMessage = logMessage.replace(/\\/g, ""); // Remove backslashes from the JSON string
+        break;
+        case "plaintext":
+        // Log in plaintext format
+        logMessage = `${logData.timestamp} [${logData.level}] ${logData.username}: ${logData.message}\n`;
+        break;
+        case "xml":
+        // Log in XML format
+        logMessage = `<log><timestamp>${logData.timestamp}</timestamp><level>${logData.level}</level><username>${logData.username}</username><message>${logData.message}</message></log>\n`;
+        break;
+        default:
+        logMessage = `${logData.timestamp} [${logData.level}] ${logData.username}: ${logData.message}\n`;
+        break;
+    }
+
+    // Append the log message to the log file
+  fs.appendFile(logFilePath, logMessage, (err) => {
+    if (err) {
+      console.error("Error logging message:", err);
+    }
+  });
+}
+
 // WebSocket server setup
 const wss = new WebSocket.Server({ port: port });
 
